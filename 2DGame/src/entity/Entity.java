c658268a690c6a5d56cbdbecfb10cae3f9876e2b
JColmenares0212 <90932504+JColmenares0212@users.noreply.github.com>
2022-06-27 19:26:14 -0500

    Update Entity.java

--- 2DGame/src/entity/Entity.java
+++ 2DGame/src/entity/Entity.java
@@ -4,7 +4,7 @@ import java.awt.image.BufferedImage;
 
 public class Entity {
 	
-	public int x, y;
+	public int worldX, worldY;
 	public int speed;
 	
 	public BufferedImage este0, este1, este2, norte0, norte1, norte2, oeste0, oeste1, oeste2, sur0, sur1, 
@@ -16,3 +16,4 @@ public class Entity {
 	public int spriteNum = 0;
 
 }
+
